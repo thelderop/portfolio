48b169e95e38c8c4127f58afc0c5462bdfff7c65
Thomas Helderop <thomashelderop@Thomass-MacBook-Air.local>
2020-08-07 17:16:14 -0600

    added random testing pages

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/navbar'
+
+export default function About() {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <h1>My name is Thomas!</h1>
+            </div>
+        </div>
+    )
+}
